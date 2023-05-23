e9bdb159dc4a82da99069e2d3cb29eff64275d75
Patryk Garwol <patrykgarwol2000@wp.pl>
2023-05-24 01:03:30 +0200

    Added thingies to scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    public static Canvas authorsCanvas;
+    public static Canvas menuCanvas;
+
+    void Awake() {
+        authorsCanvas = GameObject.Find("AuthorsCanvas").GetComponent<Canvas>();
+        menuCanvas = GameObject.Find("MenuCanvas").GetComponent<Canvas>();
+
+        authorsCanvas.enabled = false;
+    }
+
+
+    public void Play() {
+        // TODO: switch to game scene
+        SceneManager.LoadScene("PoznanPlane");
+    }
+
+    public void ShowAuthors() {
+        menuCanvas.enabled = false;
+        authorsCanvas.enabled = true;
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void QuitAuthors() {
+        menuCanvas.enabled = true;
+        authorsCanvas.enabled = false;
+    }
+}
